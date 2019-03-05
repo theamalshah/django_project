d1bce91841810dd84a35396bdd9d91aae7a9d826
theamalshah <theamalshah@gmail.com>
2019-03-05 20:05:48 +0530

    v2

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import user
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	data=user.objects.all();
+	return render(request,"myapp/home.html",{"data":data})
